d4335a609ee02ab87c067c6d946697bd0e20f150
ofadiman <ofadiman@gmail.com>
2024-01-18 00:14:16 +0100

    feature: setup fake database

---
+++ db/seed.ts
@@ -0,0 +1,18 @@
+import { writeFileSync } from "node:fs";
+import { join } from "node:path";
+import { faker } from "@faker-js/faker";
+
+const posts = [];
+for (let i = 0; i < 10; i++) {
+  posts.push({
+    id: faker.string.uuid(),
+    content: faker.lorem.sentences(3),
+    title: faker.lorem.sentences(1),
+    likes: faker.number.int({ min: 0, max: 100 }),
+  });
+}
+
+writeFileSync(
+  join(process.cwd(), "db.json"),
+  JSON.stringify({ posts }, null, 2),
+);
