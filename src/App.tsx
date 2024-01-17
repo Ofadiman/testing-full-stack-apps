82a752a3bdf3d7c4e82e8b07e2ad3e5fc2072cdf
ofadiman <ofadiman@gmail.com>
2024-01-18 00:30:52 +0100

    feature: query database for posts

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useQuery } from "react-query";
+
+const wait = (milliseconds: number) =>
+  new Promise((resolve) => {
+    setTimeout(resolve, milliseconds);
+  });
+
+const getPosts = async () => {
+  await wait(500);
+  return fetch("http://localhost:3000/posts")
+    .then((response) => response.json())
+    .then((json) => json);
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const query = useQuery("todos", getPosts);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  if (query.isLoading) {
+    return <div>loading...</div>;
+  }
+
+  if (query.isError) {
+    return <div>error...</div>;
+  }
+
+  return <pre>{JSON.stringify(query.data, null, 2)}</pre>;
 }
 
-export default App
+export default App;
