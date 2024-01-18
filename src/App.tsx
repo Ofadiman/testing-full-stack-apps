086c9e87d6e7f8a74dc65e62db34bdee79958ac7
ofadiman <ofadiman@gmail.com>
2024-01-18 08:12:30 +0100

    feature: render post cards grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
+import {
+  Card,
+  Unstable_Grid2 as Grid,
+  CardActions,
+  CardContent,
+  CardHeader,
+} from "@mui/material";
 import { useQuery } from "react-query";
+
+type Post = {
+  id: string;
+  content: string;
+  title: string;
+  likes: number;
+};
 
 const wait = (milliseconds: number) =>
   new Promise((resolve) => {
     setTimeout(resolve, milliseconds);
   });
 
-const getPosts = async () => {
+const getPosts = async (): Promise<Post[]> => {
   await wait(500);
   return fetch("http://localhost:3000/posts")
     .then((response) => response.json())
@@ -19,11 +33,25 @@ function App() {
     return <div>loading...</div>;
   }
 
-  if (query.isError) {
+  if (query.isError || query.data === undefined) {
     return <div>error...</div>;
   }
 
-  return <pre>{JSON.stringify(query.data, null, 2)}</pre>;
+  return (
+    <Grid container spacing={2} padding={2} disableEqualOverflow>
+      {query.data.map((post) => {
+        return (
+          <Grid xs={12} sm={6} md={4} lg={3} xl={2} key={post.id}>
+            <Card sx={{ height: "100%" }}>
+              <CardHeader title={post.title}></CardHeader>
+              <CardContent>{post.content}</CardContent>
+              <CardActions>Likes: {post.likes}</CardActions>
+            </Card>
+          </Grid>
+        );
+      })}
+    </Grid>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,13 @@
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "react-query";
+import CssBaseline from "@mui/material/CssBaseline";
 
 const queryClient = new QueryClient();
 
@@ -10,5 +16,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <QueryClientProvider client={queryClient}>
       <App />
     </QueryClientProvider>
+    <CssBaseline />
   </React.StrictMode>,
 );
