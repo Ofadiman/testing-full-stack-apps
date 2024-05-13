82a6ebb1bf09d9a3dc28bce290f65b5295aaaf59
ofadiman <ofadiman@gmail.com>
2024-05-13 13:48:43 +0200

    test: remove not active branch

--- src/Posts.page.tsx
+++ src/Posts.page.tsx
@@ -14,23 +14,21 @@ type Post = {
   likes: number;
 };
 
-const getPosts = async (): Promise<Post[]> => {
-  const response = await fetch("http://0.0.0.0:3000/posts", {
-    headers: {
-      Authorization: window.localStorage.getItem("token")!,
-    },
-  });
-
-  if (!response.ok) {
-    throw new Error("error from getPosts");
-  }
-
-  return response.json();
-};
-
 export function Posts() {
   const query = useQuery({
-    queryFn: getPosts,
+    queryFn: async (): Promise<Post[]> => {
+      const response = await fetch("http://0.0.0.0:3000/posts", {
+        headers: {
+          Authorization: window.localStorage.getItem("token")!,
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error("error from getPosts");
+      }
+
+      return response.json();
+    },
     queryKey: ["posts"],
     retry: false,
     onError: () => {
@@ -48,19 +46,17 @@ export function Posts() {
 
   return (
     <Grid container spacing={2} padding={2} disableEqualOverflow>
-      {query.data === undefined
-        ? "Data not available"
-        : query.data.map((post) => {
-            return (
-              <Grid xs={12} sm={6} md={4} lg={3} xl={2} key={post.id}>
-                <Card sx={{ height: "100%" }} data-cy={"card"}>
-                  <CardHeader title={post.title}></CardHeader>
-                  <CardContent>{post.content}</CardContent>
-                  <CardActions>Likes: {post.likes}</CardActions>
-                </Card>
-              </Grid>
-            );
-          })}
+      {query.data?.map((post) => {
+        return (
+          <Grid xs={12} sm={6} md={4} lg={3} xl={2} key={post.id}>
+            <Card sx={{ height: "100%" }} data-cy={"card"}>
+              <CardHeader title={post.title}></CardHeader>
+              <CardContent>{post.content}</CardContent>
+              <CardActions>Likes: {post.likes}</CardActions>
+            </Card>
+          </Grid>
+        );
+      })}
     </Grid>
   );
 }
